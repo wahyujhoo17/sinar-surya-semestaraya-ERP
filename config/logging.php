42c30edce5dd4b1d0680242c223f8e5692008f06
diianekawti <diianekawti@gmail.com>
2025-06-16 12:27:17 +0700

    update log

--- config/logging.php
+++ config/logging.php
@@ -89,8 +89,8 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
             ],
+
             'processors' => [PsrLogMessageProcessor::class],
         ],
 
