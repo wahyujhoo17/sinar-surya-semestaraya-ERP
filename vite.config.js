06496adca9bf11f34974eb3516a57d1d7a7a5f4e
diianekawti <diianekawti@gmail.com>
2025-06-16 13:45:30 +0700

    update paksa https

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        https: process.env.APP_ENV === 'production',
+        host: '0.0.0.0',
+    },
 });
