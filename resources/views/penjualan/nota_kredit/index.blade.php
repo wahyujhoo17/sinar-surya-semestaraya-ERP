69ab87500f7cafdf37c4999d3609143ee3f779b4
diianekawti <diianekawti@gmail.com>
2025-08-28 19:49:48 +0700

    Remove commented-out code for creating Nota Kredit button in the credit note index view

--- resources/views/penjualan/nota_kredit/index.blade.php
+++ resources/views/penjualan/nota_kredit/index.blade.php
@@ -14,7 +14,7 @@
                         </p>
                     </div>
                 </div>
-                <div>
+                {{-- <div>
                     @if (auth()->user()->hasPermission('nota_kredit.create'))
                         <a href="{{ route('penjualan.nota-kredit.create') }}"
                             class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
@@ -26,7 +26,7 @@
                             Buat Nota Kredit
                         </a>
                     @endif
-                </div>
+                </div> --}}
             </div>
         </div>
 
